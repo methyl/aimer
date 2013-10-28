4f10e75e0deda3f1f7fe8e7974db284cf9e31569
Lucjan Suski <lucjansuski@gmail.com>
2013-10-28 20:07:45 +0100

    Added assets to precompile

--- config/application.rb
+++ config/application.rb
@@ -76,5 +76,9 @@ module Aimer
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += [
+      "store.js"
+    ]
   end
 end
