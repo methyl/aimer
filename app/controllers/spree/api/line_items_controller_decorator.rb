49d3c8b054ea7515f90df7eff1ebc9a87997ca57
Lucjan Suski <lucjansuski@gmail.com>
2014-03-10 04:21:20 +0800

    Trying to fix invalid order params returned from line items controller

--- app/controllers/spree/api/line_items_controller_decorator.rb
+++ app/controllers/spree/api/line_items_controller_decorator.rb
@@ -4,6 +4,7 @@ Spree::Api::LineItemsController.class_eval do
     @line_item = order.contents.add(variant, params[:line_item][:quantity])
     if @line_item.save
       @order.ensure_updated_shipments
+      @order.reload
       respond_with(@order, status: 201, default_template: 'spree/api/orders/show')
     else
       invalid_resource!(@line_item)
@@ -14,7 +15,7 @@ Spree::Api::LineItemsController.class_eval do
     @line_item = order.line_items.find(params[:id])
     if @order.contents.update_cart(line_items_attributes)
       @order.ensure_updated_shipments
-      @line_item.reload
+      @order.reload
       respond_with(@order, default_template: 'spree/api/orders/show')
     else
       invalid_resource!(@line_item)
@@ -26,6 +27,7 @@ Spree::Api::LineItemsController.class_eval do
     variant = Spree::Variant.find(@line_item.variant_id)
     @order.contents.remove(variant, @line_item.quantity)
     @order.ensure_updated_shipments
+    @order.reload
     respond_with(@order, status: 200, default_template: 'spree/api/orders/show')
   end
 end
