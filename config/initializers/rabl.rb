da0572e51f249cde8bbbc607d521821bd52ae820
Lucjan Suski <lucjansuski@gmail.com>
2014-03-10 01:10:48 +0800

    Removed rabl config, trying to fix missing payment_methods

--- config/initializers/rabl.rb
+++
@@ -1,5 +0,0 @@
-Rabl.configure do |config|
-  config.cache_all_output = true
-  config.cache_sources = Rails.env.to_s != 'development' # Defaults to false
-  config.view_paths = [Rails.root.join("app/views")]
-end
