49a0db84f41b425697dee6097ca933120b3390e3
Lucjan Suski <lucjansuski@gmail.com>
2014-03-10 15:29:46 +0800

    Revert "Trying to fix invalid order params returned from line items controller"
    This reverts commit 49d3c8b054ea7515f90df7eff1ebc9a87997ca57.

--- app/controllers/spree/api/line_items_controller_decorator.rb
+++ app/controllers/spree/api/line_items_controller_decorator.rb
@@ -4,7 +4,6 @@ Spree::Api::LineItemsController.class_eval do
     @line_item = order.contents.add(variant, params[:line_item][:quantity])
     if @line_item.save
       @order.ensure_updated_shipments
-      @order.reload
       respond_with(@order, status: 201, default_template: 'spree/api/orders/show')
     else
       invalid_resource!(@line_item)
@@ -15,7 +14,7 @@ Spree::Api::LineItemsController.class_eval do
     @line_item = order.line_items.find(params[:id])
     if @order.contents.update_cart(line_items_attributes)
       @order.ensure_updated_shipments
-      @order.reload
+      @line_item.reload
       respond_with(@order, default_template: 'spree/api/orders/show')
     else
       invalid_resource!(@line_item)
@@ -27,7 +26,6 @@ Spree::Api::LineItemsController.class_eval do
     variant = Spree::Variant.find(@line_item.variant_id)
     @order.contents.remove(variant, @line_item.quantity)
     @order.ensure_updated_shipments
-    @order.reload
     respond_with(@order, status: 200, default_template: 'spree/api/orders/show')
   end
 end
