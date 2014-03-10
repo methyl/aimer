3046ea5b41992cc4d45814ff80de0b0d0601fc8d
Lucjan Suski <lucjansuski@gmail.com>
2014-03-11 00:35:01 +0800

    Patched register method to sign in user after creating

--- app/controllers/spree/api/users_controller_decorator.rb
+++ app/controllers/spree/api/users_controller_decorator.rb
@@ -1,4 +1,17 @@
 Spree::Api::UsersController.class_eval do
+  include Devise::Controllers::Helpers
+
+  def create
+    authorize! :create, Spree.user_class
+    @user = Spree.user_class.new(user_params)
+    if @user.save
+      sign_in @user
+      respond_with(@user, :status => 201, :default_template => :show)
+    else
+      invalid_resource!(@user)
+    end
+  end
+
   def current
     @user = current_api_user
     respond_with(@user)
