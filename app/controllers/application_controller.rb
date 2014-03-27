48a7e3437684cb063d39051c97b94d93e0ad17eb
Lucjan Suski <lucjansuski@gmail.com>
2014-03-27 21:39:15 +0800

    Remove ssl redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  before_filter do
-    if request.ssl? && Rails.env.production?
-      redirect_to :protocol => 'http://', :status => :moved_permanently
-    end
-  end
 end
