1d9b6c8d9819cdf62ac0a590696229ad6d8aa799
Lucjan Suski <lucjansuski@gmail.com>
2014-03-27 20:48:59 +0800

    Add assets compression and deflater

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,8 @@ module Aimer
       end
     end
 
+    config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ Aimer::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.js_compressor  = :uglifier
+  config.assets.css_compressor = :sass
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
