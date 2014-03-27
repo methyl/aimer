cc42748bf93762aaf074afcfc43594b738c967c9
Lucjan Suski <lucjansuski@gmail.com>
2014-03-27 21:07:28 +0800

    Redirect https to http

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter do
+    if request.ssl? && Rails.env.production?
+      redirect_to :protocol => 'http://', :status => :moved_permanently
+    end
+  end
 end
