d6a806ec5cc0774faed6d97329d93340cb998ac0
Lucjan Suski <lucjansuski@gmail.com>
2014-03-27 21:05:57 +0800

    Bump assets version

--- config/application.rb
+++ config/application.rb
@@ -77,7 +77,7 @@ module Aimer
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
 
     config.assets.precompile += ['store.js', 'store.css']
 
